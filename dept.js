4cdeba577b295b89c883a9732111320c2fee283e
CircleDuckFan <shangying05@gmail.com>
2012-09-05 18:35:25 +0800

    Update dept.js

--- dept.js
+++ dept.js
@@ -1,7 +1,6 @@
 var deptMap = {};
-var deptIdMap = {};
 var trimed = false;
-function getDeptDetail(key){
+function getDeptDetail(deptid){
     if(!trimed){
 	    deptMapOri = deptMap;
 		deptMap = {};
@@ -9,32 +8,15 @@ function getDeptDetail(key){
 		    var arr = jQuery.trim(deptMapOri[p]).split(',');
 			var key = jQuery.trim(p);
 			deptMap[key] = arr;
-			if(!deptIdMap[arr[2]]){
-			    deptIdMap[arr[2]] = [];
-			}
-			deptIdMap[arr[2]].push(key);
 		}
 		trimed = true;
 	}
-	if(deptMap[key]){
-	    return deptMap[key].join(', ');
+	if(deptMap[deptid]){
+	    return deptMap[deptid].join(', ');
 	}else{
 	    return '<label class=error>Not exist!</label>'
 	}
 }
-
-function getRandomDept(){
-    var deptArr = ['B','T','C','K'];
-    var random = a.getSeconds();
-	return deptArr[random%deptArr.length];
-}
-
-function getRandomDeptIdByDept(dept){
-    var deptIdArr = deptIdMap[dept]
-    var random = a.getSeconds();
-	return deptIdArr[random%deptIdArr.length];
-}
-
 //SQL: use to refre the  DB value
 //SELECT  'deptMap["', KEY_M, '"]="', VALUE_X, '";' FROM RES_CONFIG where value_x like '%,%,%' and key_m like 'P01%'
 
